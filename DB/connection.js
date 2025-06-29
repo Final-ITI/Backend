146f83d2a20438ef69320ae768f4d8194af0997a
Ali Mohamed <alimohamed11907@gmail.com>
2025-06-30 02:43:29 +0300

    fix(db): Export connectionDB function and enhance error handling

--- DB/connection.js
+++ DB/connection.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
-const connectionDB = async () => {
+export const connectionDB = async () => {
   return await mongoose
-    .connect(process.env.DB_URI)
+    .connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,
+    })
     .then(() => console.log("DB Connected Successfully"))
-    .catch(() => console.log("DB Not Connected"));
+    .catch((error) => {
+      console.log("DB Not Connected", error);
+    });
 };
-
-export default connectionDB;
