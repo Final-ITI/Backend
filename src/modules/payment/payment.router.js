0a22a46fad3bd767b40844304aad9d0cb7cbd678
Ali Mohamed <alimohamed11907@gmail.com>
2025-07-22 17:50:34 +0300

    Refactor payment router to apply authentication and authorization middleware correctly

--- src/modules/payment/payment.router.js
+++ src/modules/payment/payment.router.js
@@ -4,8 +4,8 @@ import { initiatePayment, paymobPaymentWebhook } from "./payment.controller.js";
 
 const router = Router();
 
-router.use(authenticate, authorize("student"));
+
 // /api/v1/payment/initiate - Initiate payment for enrollment
-router.post('/initiate',initiatePayment)
+router.post('/initiate',authenticate, authorize("student"),initiatePayment)
 router.post("/webhook/paymob", paymobPaymentWebhook);
 export default router;
