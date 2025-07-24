b63c5431e8cad998eaa3634ac214be3d50271d6c
Ali Mohamed <alimohamed11907@gmail.com>
2025-07-24 08:17:20 +0300

    Add payment validation and enhance payment initiation logic for wallet support and end paymentMethod wallet

--- src/modules/payment/payment.router.js
+++ src/modules/payment/payment.router.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authenticate, authorize } from "../../middlewares/auth.middleware.js";
 import { initiatePayment, paymobPaymentWebhook } from "./payment.controller.js";
+import { validate } from "../../middlewares/validation.middleware.js";
+import { initiatePaymentValidation } from "./payment.validation.js";
 
 const router = Router();
 
 
 // /api/v1/payment/initiate - Initiate payment for enrollment
-router.post('/initiate',authenticate, authorize("student"),initiatePayment)
+router.post('/initiate',authenticate, authorize("student"), validate(initiatePaymentValidation),initiatePayment)
 router.post("/webhook/paymob", paymobPaymentWebhook);
 export default router;
